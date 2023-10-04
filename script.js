b11a04011170a61bf6fb432ebc9d36f20b93f6bc
lakuna2 <auliaadeputri2003@gmail.com>
2023-10-04 15:43:55 +0800

    alhamdulillah

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
   // ---------------------------------------------------------------------------------------- //
-  const API_BASE_URL = 'https://railway-booking-production.up.railway.app';
+  const API_BASE_URL = 'https://be-balikpapan-29-production.up.railway.app';
 
   const bookedTimes = {}; // Objek untuk melacak waktu yang sudah terpakai di hari yang sama
   
@@ -91,6 +91,32 @@ document.addEventListener("DOMContentLoaded", function () {
   
   // Mengambil data reservasi saat halaman dimuat
   window.addEventListener('DOMContentLoaded', fetchData);
+  
+  // Fungsi untuk menampilkan notifikasi
+  function showNotification(title, message) {
+    if ('Notification' in window) {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          const notification = new Notification(title, {
+            body: message,
+            icon: "path/to/icon.png" // Ganti dengan URL ikon Anda
+          });
+  
+          // Bisa menambahkan event click untuk menangani tindakan lebih lanjut
+          notification.onclick = function () {
+            // Lakukan tindakan setelah notifikasi diklik (jika diperlukan)
+          };
+        } else {
+          // Notifikasi ditolak
+          // Anda dapat menampilkan pesan alternatif jika notifikasi tidak diizinkan
+          alert(message);
+        }
+      });
+    } else {
+      // Browser tidak mendukung notifikasi, tampilkan pesan alternatif
+      alert(message);
+    }
+  }
   
   // Mendefinisikan fungsi untuk mengirim data reservasi ke API
   async function submitForm(event) {
@@ -111,9 +137,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const selectedDate = bookingData.reservationdate;
     const selectedTime = bookingData.reservationtime;
   
-    // Memeriksa apakah waktu yang dipilih sudah terpakai di hari yang sama
-    if (bookedTimes[selectedDate] && bookedTimes[selectedDate].includes(selectedTime)) {
-      alert('Waktu yang dipilih sudah terpakai. Silakan pilih waktu yang lain.');
+    // Memeriksa apakah kombinasi service, dokter, tanggal, dan waktu sudah ada dalam reservasi yang ada
+    const isDuplicateBooking = await isBookingDuplicate(bookingData);
+    if (isDuplicateBooking) {
+      alert('Reservasi dengan kombinasi yang sama sudah ada. Silakan pilih kombinasi yang lain.');
       return;
     }
   
@@ -134,41 +161,59 @@ document.addEventListener("DOMContentLoaded", function () {
           bookedTimes[selectedDate] = [];
         }
         bookedTimes[selectedDate].push(selectedTime);
+  
+        // Tampilkan notifikasi berhasil
+        showNotification('Reservasi berhasil!', 'Terima kasih telah melakukan reservasi.');
       } else {
         console.error('Gagal membuat reservasi');
+        // Tampilkan notifikasi gagal
+        showNotification('Gagal membuat reservasi', 'Mohon coba lagi nanti.');
       }
     } catch (error) {
       console.error(error);
+      // Tampilkan notifikasi gagal
+      showNotification('Gagal membuat reservasi', 'Terjadi kesalahan. Mohon coba lagi nanti.');
     }
   }
   
-
-// Mendefinisikan fungsi untuk menampilkan data reservasi ke dalam tabel
-function displayBooking(booking) {
-  const row = document.createElement('tr');
-
-  // Mengambil tanggal dari booking.reservationdate (format: yyyy-mm-dd)
-  const reservationDate = booking.reservationdate.substring(0, 10); // Ambil tanggalnya saja
-  const dateParts = reservationDate.split('-');
-  const formattedDate = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`; // Format ulang menjadi dd-mm-yyyy
-
-  row.innerHTML = `
-    <td>${booking.name}</td>
-    <td>${booking.email}</td>
-    <td>${booking.phone}</td>
-    <td>${booking.service}</td>
-    <td>${booking.doctor}</td>
-    <td>${formattedDate}</td>
-    <td>${booking.reservationtime}</td>
-  `;
-
-
-  resultTable.appendChild(row);
-
-  // Setelah menampilkan data, kosongkan formulir untuk reservasi berikutnya jika perlu
-  reservationForm.reset();
-}
-
+  // Mendefinisikan fungsi untuk menampilkan data reservasi ke dalam tabel
+  function displayBooking(booking) {
+    const row = document.createElement('tr');
+  
+    // Memeriksa apakah properti reservationdate ada dalam objek booking
+    if (booking.reservationdate) {
+      // Mengambil tanggal dari booking.reservationdate (format: yyyy-mm-dd)
+      const reservationDate = booking.reservationdate.substring(0, 10); // Ambil tanggalnya saja
+      const dateParts = reservationDate.split('-');
+      const formattedDate = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`; // Format ulang menjadi dd-mm-yyyy
+  
+      row.innerHTML = `
+        <td>${booking.name}</td>
+        <td>${booking.email}</td>
+        <td>${booking.phone}</td>
+        <td>${booking.service}</td>
+        <td>${booking.doctor}</td>
+        <td>${formattedDate}</td>
+        <td>${booking.reservationtime}</td>
+      `;
+    } else {
+      // Jika properti reservationdate tidak ada, set tampilan kolom reservationdate menjadi kosong
+      row.innerHTML = `
+        <td>${booking.name}</td>
+        <td>${booking.email}</td>
+        <td>${booking.phone}</td>
+        <td>${booking.service}</td>
+        <td>${booking.doctor}</td>
+        <td></td>
+        <td>${booking.reservationtime}</td>
+      `;
+    }
+  
+    resultTable.appendChild(row);
+  
+    // Setelah menampilkan data, kosongkan formulir untuk reservasi berikutnya jika perlu
+    // reservationForm.reset();
+  }
   
   // Mendefinisikan fungsi untuk mengambil data reservasi dari API
   async function fetchData() {
@@ -192,5 +237,30 @@ function displayBooking(booking) {
     } catch (error) {
       console.error(error);
     }
+  }
+  
+  // Fungsi untuk memeriksa apakah kombinasi reservasi sudah ada
+  async function isBookingDuplicate(newBooking) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/booking`);
+      if (response.ok) {
+        const bookings = await response.json();
+        for (const booking of bookings) {
+          if (
+            newBooking.service === booking.service &&
+            newBooking.doctor === booking.doctor &&
+            newBooking.reservationdate === booking.reservationdate &&
+            newBooking.reservationtime === booking.reservationtime
+          ) {
+            return true; // Jika ada kombinasi yang sama, kembalikan true
+          }
+        }
+      } else {
+        console.error('Gagal mengambil data reservasi');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    return false; // Jika tidak ada kombinasi yang sama, kembalikan false
   }
   
